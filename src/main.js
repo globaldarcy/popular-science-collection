817e90b8141ce74f679d57fe0fc5a6c3b3367a19
Darcy <289904316@qq.com>
2020-07-10 15:43:48 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios);
+Vue.use(ElementUI);
 
 new Vue({
   render: h => h(App),
